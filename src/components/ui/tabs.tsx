fe122d97992e171b61067f286e15dd2be05adf9b
Thomas Ghysels <info@thomasg.be>
2024-01-12 16:18:40 +0100

    Fix focus visible ring on modal

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -44,7 +44,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      'mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
+      'mt-2 ring-offset-white focus-visible:outline-none dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
       className
     )}
     {...props}
