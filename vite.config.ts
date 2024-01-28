00f4cbe0f8de1876100d82b780fc620e8a185b17
Thomas Ghysels <info@thomasg.be>
2024-01-28 17:31:55 +0100

    Resolve console warnings during build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    muteWarningsPlugin([
+      ['SOURCEMAP_ERROR', "Can't resolve original location of error"],
+      // [
+      //   'INVALID_ANNOTATION',
+      //   'contains an annotation that Rollup cannot interpret',
+      // ],
+    ]),
+  ],
   build: {
-    chunkSizeWarningLimit: 600,
+    chunkSizeWarningLimit: 700,
   },
   resolve: {
     alias: {
@@ -14,3 +23,45 @@ export default defineConfig({
     },
   },
 })
+
+function muteWarningsPlugin(warningsToIgnore: string[][]): Plugin {
+  const mutedMessages = new Set()
+  return {
+    name: 'mute-warnings',
+    enforce: 'pre',
+    config: (userConfig) => ({
+      build: {
+        rollupOptions: {
+          onwarn(warning, defaultHandler) {
+            if (warning.code) {
+              const muted = warningsToIgnore.find(
+                ([code, message]) =>
+                  code == warning.code && warning.message.includes(message)
+              )
+
+              if (muted) {
+                mutedMessages.add(muted.join())
+                return
+              }
+            }
+
+            if (userConfig.build?.rollupOptions?.onwarn) {
+              userConfig.build.rollupOptions.onwarn(warning, defaultHandler)
+            } else {
+              defaultHandler(warning)
+            }
+          },
+        },
+      },
+    }),
+    closeBundle() {
+      const diff = warningsToIgnore.filter((x) => !mutedMessages.has(x.join()))
+      if (diff.length > 0) {
+        this.warn(
+          'Some of your muted warnings never appeared during the build process:'
+        )
+        diff.forEach((m) => this.warn(`- ${m.join(': ')}`))
+      }
+    },
+  }
+}
